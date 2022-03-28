f5922f07c85f4f6e7664edddff7601cb048a9f28
harris-chris <chrisharriscjh@gmail.com>
2022-03-29 08:11:35 +0900

    Impair flake.nix for cbonsai

--- cbonsai_build/flake.nix
+++ cbonsai_build/flake.nix
@@ -1,36 +1,35 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        packages = {
-          cbonsai = pkgs.stdenvNoCC.mkDerivation {
-            name = "cbonsai";
-            src = pkgs.fetchFromGitLab {
-              owner = "jallbrit";
-              repo = "cbonsai";
-              rev = "b3ee97a0";
-              sha256 = "8JwwrTS8pOLwNYsnBGwqazGrTbts7LADndEdTit6Kc0=";
-            };
-            nativeBuildInputs = with pkgs; [ gcc gnumake pkg-config ];
-            buildInputs = with pkgs; [ ncurses ]; #makeWrapper
-            installPhase = ''
-              make install PREFIX=$out
-            '';
-            # preFixup = ''
-            #   wrapProgram "$out/bin/cbonsai" --add-flags "--live"
-            # '';
+
+        cbonsai = pkgs.stdenvNoCC.mkDerivation {
+          name = "cbonsai";
+          src = pkgs.fetchFromGitLab {
+            owner = "jallbrit";
+            repo = "cbonsai";
+            rev = "b3ee97a0";
+            sha256 = "8JwwrTS8pOLwNYsnBGwqazGrTbts7LADndEdTit6Kc0=";
           };
+          # nativeBuildInputs = with pkgs; [ gcc gnumake pkg-config ];
+          # buildInputs = with pkgs; [ ncurses ]; #makeWrapper
+          installPhase = ''
+            make install PREFIX=$out
+          '';
+          # preFixup = ''
+          #   wrapProgram "$out/bin/cbonsai" --add-flags "--live"
+          # '';
         };
       in rec {
         devShell = pkgs.mkShell {
-          buildInputs = [ packages.cbonsai ];
+          buildInputs = [ cbonsai ];
         };
-        defaultPackage = packages.cbonsai;
+        # defaultPackage = cbonsai;
       });
 }
 
